cabd544c116d5886bdd3777e092a625ff76e19a2
ignacio-magno <88296440+ignacio-magno@users.noreply.github.com>
2025-03-30 02:57:37 -0300

    test.

---
+++ core/src/test/java/TestMain.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class TestMain {
+
+    @Test
+    public void testMain() {
+        System.out.println("TestMain");
+        assert "TestMain".equals("TestMain");
+    }
+}
