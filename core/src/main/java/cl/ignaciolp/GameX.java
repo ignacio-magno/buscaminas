a55f0d2755daad390dae09d3fcc1395438de049e
ignacio-magno <88296440+ignacio-magno@users.noreply.github.com>
2025-03-30 21:57:57 -0300

    clean imports

--- core/src/main/java/cl/ignaciolp/GameX.java
+++ core/src/main/java/cl/ignaciolp/GameX.java
@@ -1,15 +1,9 @@
 package cl.ignaciolp;
 
-import Logic.Cell;
 import Logic.Tablero;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -50,7 +44,6 @@ public class GameX extends ScreenAdapter {
     private void DrawTablero() {
         for (int i = 0; i < tablero.getWidth(); i++) {
             for (int j = 0; j < tablero.getHeight(); j++) {
-                Logic.Cell cell = tablero.Cell(i, j);
                 CellActor actor = new CellActor(texture, i, j, tablero);
                 stage.addActor(actor);
             }
